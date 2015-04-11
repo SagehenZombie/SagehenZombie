87bfaf90230f14464487790f3b93a30faf89a5f1
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-04-11 00:29:57 -0700

    Write socket.io on play page

---
+++ public/javascript/play_io.js
@@ -0,0 +1,29 @@
+window.onload=function(){
+
+    function getUrlParameter(sParam)
+    {
+        var sPageURL = window.location.search.substring(1);
+        var sURLVariables = sPageURL.split('&');
+        for (var i = 0; i < sURLVariables.length; i++)
+        {
+            var sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] == sParam)
+            {
+                return sParameterName[1];
+            }
+        }
+    }
+
+    var id = getUrlParameter('id');
+    var socket = io.connect('http://localhost:3000');
+    socket.on('welcome',function(data){
+        players = data.players;
+    });
+    for(i in players){
+        if(players[i].id = id){
+            player = players[i];
+        }
+    }
+
+
+}
---
+++ public/javascript/user_io.js
@@ -0,0 +1,61 @@
+var ready = false;
+window.onload=function(){
+
+    $('#sel').change(function(){
+        if($(this).val()=='human'){
+            $('#demo_img').attr('src','/images/sagehen_front.png');
+        }
+        else{
+            $('#demo_img').attr('src','/images/zombie_front.png');
+        }
+    })
+
+    var socket = io.connect('http://localhost:3000');
+
+    socket.on('welcome',function(data){
+        $('#state').text('Successfully connected to the server!');
+        var players = data.players;
+        for(i in players){
+            $('#players ul').append('<li>'+players[i].avatar+' '+players[i].name+'</li>');
+        }
+    });
+
+    $('#ready_button').click(function(){
+        if(!ready){
+            ready = true;
+            $(this).text('Waiting');
+            $('#state').text('Waiting for more players');
+            var name = $('#nm').val();
+            var avatar = $('#sel').val();
+            socket.emit('connected',{name:name,avatar:avatar});
+            $('#players ul').append('<li>'+avatar+' '+name+'</li>');}
+    });
+
+
+    socket.on('countdown',function(){
+        seconds = 30;
+        $('#state').text('Countdown: '+seconds);
+        setInterval(function(){
+            seconds--;
+            $('#state').text('Countdown: '+seconds);
+        },1000)
+    });
+
+    socket.on('newPlayer',function(data){
+        newPlayer = data.player;
+        $('#players ul').append('<li>'+newPlayer.avatar+' '+newPlayer.name+'</li>');
+    });
+
+    var id;
+    socket.on('id',function(data){
+        id = data.id;
+    });
+
+    socket.on('game start',function(){
+        if(!ready){
+            $('#ready_button').trigger('click');
+        }
+        window.location= '/?id='+id;
+    });
+}
+
